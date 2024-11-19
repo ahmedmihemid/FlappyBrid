c8dd6e764defb50ad9bce8463a8cd42016fd0405
ahmedmihemid <147274315+ahmedmihemid@users.noreply.github.com>
2024-11-19 18:05:46 +0300

    creeted the main project

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FlappyBridGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Image = global::FlappyBridGame.Properties.Resources.NicePng_flappy_bird_pipe_png_1831944_removebg_preview;
+            this.pictureBox6.Location = new System.Drawing.Point(229, 142);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(59, 56);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::FlappyBridGame.Properties.Resources.pngwing_com_removebg_preview;
+            this.pictureBox5.Location = new System.Drawing.Point(267, 290);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(77, 189);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FlappyBridGame.Properties.Resources.pngwing_com_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(803, 263);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(75, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::FlappyBridGame.Properties.Resources.pngwing2;
+            this.pictureBox4.Location = new System.Drawing.Point(521, -83);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(72, 186);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::FlappyBridGame.Properties.Resources.pngwing2;
+            this.pictureBox3.Location = new System.Drawing.Point(-12, -45);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(77, 188);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::FlappyBridGame.Properties.Resources.Ground_PNG_Picture_1;
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 363);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(835, 89);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 37);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Score  0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 60;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(826, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace FlappyBridGame
+{
+    public partial class Form1 : Form
+    {
+
+        bool GameMode = true;
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void Start()
+        {
+
+
+        }
+
+
+
+
+     
+
+        private void MovePipes()
+        {
+            pictureBox1.Left -= 13;
+            pictureBox5.Left -= 13;
+            pictureBox3.Left -= 13;
+            pictureBox4.Left -= 13;
+
+            if (pictureBox1.Left < -50)
+            {
+                score++;
+                UpdateScore();
+                pictureBox1.Left = 850;
+                pictureBox1.Location = new Point(pictureBox1.Left, GetRandomSize(1));
+            }
+
+            if (pictureBox5.Left < -50)
+            {
+                score++;
+                UpdateScore();
+                pictureBox5.Left = 850;
+                pictureBox5.Location = new Point(pictureBox5.Left, GetRandomSize(2));
+            }
+
+
+            if (pictureBox3.Left < -50)
+            {
+                score++;
+                UpdateScore();
+                pictureBox3.Left = 850;
+                pictureBox3.Location = new Point(pictureBox3.Left, GetRandomSize(3));
+            }
+
+            if (pictureBox4.Left < -50)
+            {
+                score++;
+                UpdateScore();
+                pictureBox4.Left = 850;
+                pictureBox4.Location = new Point(pictureBox4.Left, GetRandomSize(4));
+            }
+
+
+        }
+
+
+        private bool isTouchPipeOrGround()
+        {
+         if( pictureBox6.Bounds.IntersectsWith(pictureBox1.Bounds) || pictureBox6.Bounds.IntersectsWith(pictureBox3.Bounds) || pictureBox6.Bounds.IntersectsWith(pictureBox4.Bounds)  || pictureBox6.Bounds.IntersectsWith(pictureBox5.Bounds)  || pictureBox6.Bounds.IntersectsWith(pictureBox2.Bounds))
+            { return true; }
+         return false;  
+        }
+
+
+        private bool MoveBird()
+        {
+            pictureBox6.Top += 5;
+            pictureBox6.Image = Properties.Resources.NicePng_flappy_bird_pipe_png_1831944_removebg_preview;
+            if (isTouchPipeOrGround()) { return false; }
+            return true;
+        }
+
+        private int GetRandomSize(int picNo)
+        {
+            Random rand = new Random();
+            int randSize;
+            switch (picNo)
+            {
+                case 1: randSize = rand.Next(184, 249); break;
+                case 2: randSize = rand.Next(184, 249); break;
+                case 3: randSize = rand.Next(-106, -1); break;
+                case 4: randSize = rand.Next(-106, -1); break;
+                default : randSize = 0; break;
+            }
+            return randSize;
+        }
+
+
+
+        private void UpdateScore()
+        {
+            label1.Text = "Score " + score.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            pictureBox6.Top -= 70;
+            pictureBox6.Image = Properties.Resources.NicePng_flappy_bird_png_1515878_removebg_preview__1_;
+           
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            MovePipes();
+            GameMode = MoveBird();
+       
+            if (!GameMode)
+            {
+                timer2.Enabled = false;
+                this.Close();
+                UpdateScore();
+            }
+        }
+    }
+}
